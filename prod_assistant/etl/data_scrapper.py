35e2ba984bb9d27416f4de43bf570320f6ab2d98
malleswarigelli <malleswari.gelli@gmail.com>
2026-07-19 06:53:22 -0500

    updated code

--- prod_assistant/etl/data_scrapper.py
+++ prod_assistant/etl/data_scrapper.py
@@ -23,7 +23,7 @@ class FlipkartScraper:
         options.add_argument("--disable-gpu")
         options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36")
         
-        driver = uc.Chrome(options=options, use_subprocess=True)
+        driver = uc.Chrome(version_main=147, options=options, use_subprocess=True)
         
         details = {
             "rating": "N/A",
@@ -96,7 +96,7 @@ class FlipkartScraper:
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-blink-features=AutomationControlled")
         
-        driver = uc.Chrome(options=options, use_subprocess=True)
+        driver = uc.Chrome(version_main=147, options=options, use_subprocess=True)
         search_url = f"https://www.flipkart.com/search?q={query.replace(' ', '+')}"
         
         print(f"Opening search URL: {search_url}")
